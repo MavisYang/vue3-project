f0522acb80cbc58c010797fe2aa648c50d384961
mavis <miaomiao.yang@gemii.cc>
2021-07-22 11:22:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: yangmiaomiao
+ * @Date: 2021-07-22 11:10:32
+ * @LastEditors: yangmiaomiao
+ * @LastEditTime: 2021-07-22 11:14:50
+ * @Description: 
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
